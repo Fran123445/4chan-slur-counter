b7dcd6facb82d84132657811ac8c39a7b55ab5bf
Franco <fralstazzone@gmail.com>
2022-09-10 22:29:40 -0300

    Added boards list

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
+import bs4
 
-def get_html_files():
+def get_html_files(boards):
 
 
 if __name__ == '__main__':
-    dict_of_files = get_html_files()
+    boards = ["3", "a", "adv", "an", "asp", "b", "bant", "biz", "c", "cgl", "ck", "cm", "co", "d", "diy",
+              "e", "f", "fa", "fit", "g", "gd", "gif", "h", "hc", "hm", "hr", "i", "ic", "int", "jp",
+              "k", "lgbt", "lit", "m", "mlp", "mu", "n", "new", "o", "out", "p", "po", "pol", "r",
+              "r9k", "s4s", "s", "sci", "soc", "sp", "t", "tg", "toy", "trv", "tv", "u", "v", "vg",
+              "vp", "vr", "vt", "w", "wg", "wsg", "wsr", "x", "y"]
+    
+    dict_of_files = get_html_files(boards)
